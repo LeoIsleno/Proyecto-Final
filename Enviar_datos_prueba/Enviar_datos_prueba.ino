a19ba4608de347100bb2933360aee14d5d326547
Leo Isleño <leoislenoxd@gmail.com>
2022-08-03 14:32:31 -0300

    Prueba Funcionando

--- Enviar_datos_prueba/Enviar_datos_prueba.ino
+++ Enviar_datos_prueba/Enviar_datos_prueba.ino
@@ -1,3 +1,4 @@
+//#include <ESP8266WiFi.h> // Para el ESP8266
 #include <WiFi.h> // Para el ESP32
 WiFiClient WIFI_CLIENT;
 #include <PubSubClient.h>
@@ -6,14 +7,19 @@ PubSubClient MQTT_CLIENT;
 const char* ssid = "Leouu";
 const char* password = "leo12345A";
 
-void setup() {
-Serial.begin(115200);
-delay(10);
-Serial.println();
-Serial.print("Connecting with ");
-Serial.println(ssid);
+// const byte led4 = D4; // LED ESP8266
+const byte LED2 = 5; // LED ESP32
 
-WiFi.begin(ssid, password);
+void setup() {
+  pinMode(LED2, OUTPUT);
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting with ");
+  Serial.println(ssid);
+
+  
+  WiFi.begin(ssid, password);
 
 while (WiFi.status() != WL_CONNECTED) {
 delay(500);
@@ -24,28 +30,30 @@ Serial.println("");
 Serial.print("WiFi conected. IP: ");
 Serial.println(WiFi.localIP());
 
+// Setting Callback.
+  MQTT_CLIENT.setCallback(callback);
 }
 
+// What to do when it receives the data. 
+void callback(char* recibido, byte* payload, unsigned int length) {
+  Serial.print("Message received: ");
+  Serial.print(recibido);
+  Serial.print("   ");
+  for (int i=0;i<length;i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
+  if (receivedChar == '1') {digitalWrite(LED2, HIGH);}
+  if (receivedChar == '0') {digitalWrite(LED2, LOW);}
+  }
+  Serial.println();
+}
+ 
 void loop() {
-// Check MQTT Broker connection,
-// en caso de que no tenga, reconecta.
-if (!MQTT_CLIENT.connected()) {
-reconnect();
+  if (!MQTT_CLIENT.connected()) {
+    reconnect();
+  }
+  MQTT_CLIENT.loop(); // Check Subscription.
 }
-
-// Publish topic.
-// Convierte el entero a char. Debe ser char.
-int aleatorio = random(1,90);
-String aleatorioString = String(aleatorio);
-char alea[6];
-aleatorioString.toCharArray(alea, 6);
-
-MQTT_CLIENT.publish("Leo/aleatorio", alea);
-
-// Wait 5 s.
-delay(5000);
-}
-
 
 // Reconecta con MQTT broker
 void reconnect() {
@@ -57,6 +65,7 @@ MQTT_CLIENT.setClient(WIFI_CLIENT);
 while (!MQTT_CLIENT.connected()) {
 Serial.println("Trying to connect with Broker MQTT.");
 MQTT_CLIENT.connect("LeoIsleno"); // it isn't necessary..
+MQTT_CLIENT.subscribe("Leo/boton"); // HERE SUBSCRIBE.
 
 // Wait to try to reconnect again...
 delay(3000);
